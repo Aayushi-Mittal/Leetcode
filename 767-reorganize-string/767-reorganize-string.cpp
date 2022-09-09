9f341428cdf32f805eb45531d00e4984b34e4101
Aayushi Mittal <aayushimittal088@gmail.com>
2022-09-10 01:00:20 +0530

    Time: 9 ms (10.26%), Space: 6.4 MB (35.76%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.cpp
+++ 767-reorganize-string/767-reorganize-string.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        map<int, int> cnts;
-        for (char c : s) cnts[c]++;
-        priority_queue<pair<int, int>> pq;
-        for (auto p : cnts)
-            pq.push({p.second, p.first});
-
-        string res;
-        pair<int, int> top1, top2;
-        while (!pq.empty()) {
-            top1 = pq.top(); pq.pop();
-            res += top1.second;
-            if (!pq.empty()) {
-                res += pq.top().second;
-                top2 = pq.top(); pq.pop();
-                if (top2.first > 1) pq.push({ top2.first - 1, top2.second });
+        string res="";
+        map<char,int> count;
+        for(int i=0; i<s.size(); i++)
+            count[s[i]]++;
+        priority_queue<pair<int,char>> pq;
+        for(auto c:count)
+            pq.push({c.second, c.first});
+        
+        pair<int,char> p1, p2;
+        while(pq.empty()!=true)
+        {
+            p1=pq.top();
+            pq.pop();
+            res+=p1.second;
+            if(!pq.empty()){
+                p2=pq.top(); pq.pop();
+                res+=p2.second;
+                if(p2.first>1)
+                    pq.push({p2.first-1, p2.second});
             }
-            if (top1.first > 1) pq.push({ top1.first - 1, top1.second });
+            if(p1.first>1)
+                pq.push({p1.first-1, p1.second});
         }
-
         for (int i = 1; i < res.size(); i++)
             if (res[i] == res[i - 1]) return "";
         return res;
