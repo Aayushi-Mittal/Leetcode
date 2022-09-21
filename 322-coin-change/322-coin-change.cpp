6f46c01f99b6109c2cf277892c884fd08f24558b
Aayushi Mittal <aayushimittal088@gmail.com>
2022-09-21 17:13:11 +0530

    Time: 85 ms (89.58%), Space: 15.5 MB (40.80%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,43 +2,24 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount+1,-1);
+        dp[0]=0;
         int ans=min_coins(coins, amount, dp);
         if(ans==INT_MAX) return -1;
-        else return ans;
+        return ans;
     }
     int min_coins(vector<int> &coins, int amount, vector<int> &dp)
     {
-        // if(amount==0) return 0;
-        // if(amount<0) return INT_MAX;
-        // if(dp[amount]!=-1) return dp[amount];
-        // dp[amount]=INT_MAX;
-        // for(int i=0; i<coins.size(); i++)
-        // {
-        //     int subans=min_coins(coins, amount-coins[i], dp);
-        //     // if(dp[amount-coins[i]]!=-1)
-        //     //     ans=dp[amount-coins[i]];
-        //     // else
-        //     if(subans!=INT_MAX)
-        //     dp[amount]=min(subans+1, dp[amount]);
-        //     // if(ans!=INT_MAX) ans+=1;
-        // }
-        // return dp[amount];
-        
-        //Base Case
-        if(amount == 0)
-            return 0;
-        if(amount < 0)
-            return INT_MAX;
-        
-        if(dp[amount] != -1) return dp[amount];
-        
-        int ans = INT_MAX;
-        for(int i = 0; i < coins.size(); i++){
-            int cur =min_coins(coins, amount - coins[i], dp);
-            if(cur != INT_MAX)
-                ans = min(ans, 1 + cur);
+        if(amount==0) return 0;
+        if(amount<0) return INT_MAX;
+        if(dp[amount]!=-1) return dp[amount];
+        int ans=INT_MAX;
+        for(int i=0; i<coins.size(); i++)
+        {
+            int subans=0;
+            subans=min_coins(coins, amount-coins[i], dp);
+            if(subans!=INT_MAX && subans+1<ans) ans=subans+1;
         }
-        return dp[amount]=ans;
+        dp[amount]=ans;
+        return ans;
     }
-    
 };
