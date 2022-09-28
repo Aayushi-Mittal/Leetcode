cc76b83638a4cf9e68fcd16f828c964ae30b0136
Aayushi Mittal <aayushimittal088@gmail.com>
2022-09-28 14:01:55 +0530

    Time: 4 ms (78.73%), Space: 10.6 MB (75.00%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -15,7 +15,7 @@ public:
         for(int i=0; i<n; i++) fast=fast->next;
         if(fast==NULL) return head->next;
         while(fast->next!=NULL){
-            fast=fast->next; 
+            fast=fast->next;
             slow=slow->next;
         }
         slow->next=slow->next->next;
