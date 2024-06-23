6720546e3f63efa64c8edefbc2df74f99750a044
Aayushi Mittal <aayushimittal088@gmail.com>
2024-06-23 23:06:59 +0530

    Time: 253 ms (14.29%), Space: 132.3 MB (28.57%) - LeetHub

--- 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -1,49 +1,21 @@
 class Solution {
 public:
     int minimumArea(vector<vector<int>>& grid) {
-        int x1=0, x2=0, y1=0, y2=0;
+        int x1=INT_MAX, x2=0, y1=INT_MAX, y2=0;
         for(int i=0; i<grid.size(); i++)
         {
             for(int j=0; j<grid[i].size(); j++)
             {
                 if(grid[i][j]==1) {
-                    x1=i;
-                    break;
+                    x1=min(x1,i);
+                    x2=max(x2,i);
+                    y1=min(y1,j);
+                    y2=max(y2,j);
                 }
             }
         }
-        for(int i=0; i<grid[0].size(); i++)
-        {
-            for(int j=0; j<grid.size(); j++)
-            {
-                if(grid[j][i]==1) {
-                    y1=i;
-                    break;
-                }
-            }
-        }
-        for(int i=grid.size()-1; i>=0; i--)
-        {
-            for(int j=grid[0].size()-1; j>=0; j--)
-            {
-                if(grid[i][j]==1) {
-                    x2=i;
-                    break;
-                }
-            }
-        }
-        for(int i=grid[0].size()-1; i>=0; i--)
-        {
-            for(int j=grid.size()-1; j>=0; j--)
-            {
-                if(grid[j][i]==1) {
-                    y2=i;
-                    break;
-                }
-            }
-        }
-        cout<<x1<<x2<<y1<<y2<<endl;
-        int ans = (abs(x2-x1)+1)*(abs(y2-y1)+1);
+        // cout<<x1<<x2<<y1<<y2<<endl;
+        int ans = (x2-x1+1)*(y2-y1+1);
         return ans;
     }
 };
