f71ba4742a7bbde254ef421bc60af328f6f90c76
Aayushi Mittal <aayushimittal088@gmail.com>
2024-09-15 05:02:21 +0530

    Time: 3 ms (70.29%), Space: 9.3 MB (80.24%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1, 0);
+        for(int i = 1; i<=n; ++i)
+            ans[i] = ans[i/2] + i%2;
+        return ans;
+    }
+};
