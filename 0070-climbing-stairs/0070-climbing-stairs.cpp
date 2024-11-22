df6edd4f30467c6bbe63c9739422f3c56227ee7f
Aayushi Mittal <aayushimittal088@gmail.com>
2024-11-23 02:15:00 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (66.54%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int dp[n+1];
+        if(n==1) return 1;
+        if(n==2) return 2;
+        dp[0]=0; dp[1]=1; dp[2]=2;
+        for(int i=3; i<n+1; i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
